6d00cd21b2b356b2950b0e962649216f29874971
Alex <aeversme@gmail.com>
2021-02-03 23:06:40 -0500

    add playlist creation functionality

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ import requests
 import secrets
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from pprint import pprint
 
 
 # date = input('What date do you want to travel to? Use the format YYYY-MM-DD: ')
 date = '2006-06-01'
 url = f'https://www.billboard.com/charts/hot-100/{date}'
 
-response = requests.get(url)
-website_html = response.text
-
-soup = BeautifulSoup(website_html, 'html.parser')
-song_titles_span = soup.find_all(name='span', class_='chart-element__information__song')
-
-song_titles = [title.getText() for title in song_titles_span]
+# response = requests.get(url)
+# website_html = response.text
+#
+# soup = BeautifulSoup(website_html, 'html.parser')
+# song_titles_span = soup.find_all(name='span', class_='chart-element__information__song')
+#
+# song_titles = [title.getText() for title in song_titles_span]
 
 scope = 'playlist-modify-private'
 
@@ -30,19 +29,25 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=secrets.SPOTIPY_CLIENT_
 user_id = sp.current_user()['id']
 
 
-def song_search():
-    year = date[:4]
-    song_uris = []
-    for i in range(len(song_titles)):
-        song_search_string = f'track: {song_titles[i]} year: {year}'
-        result = sp.search(q=song_search_string)
-        try:
-            song_uri = result['tracks']['items'][0]['uri']
-        except IndexError:
-            print(f'{song_titles[i]} not found.')
-        else:
-            song_uris.append(song_uri)
-    return song_uris
+# def song_search():
+#     year = date[:4]
+#     song_uri_list = []
+#     for song in song_titles:
+#         song_search_string = f'track: {song} year: {year}'
+#         result = sp.search(q=song_search_string, type='track')
+#         try:
+#             song_uri = result['tracks']['items'][0]['uri']
+#         except IndexError:
+#             print(f'{song} not found on Spotify. Skipped')
+#         else:
+#             song_uri_list.append(song_uri)
+#     return song_uri_list
+#
+#
+# song_uris = song_search()
 
+playlist_name = f'{date} Billboard Hot 100'
 
-pprint(song_search())
+create_playlist_response = sp.user_playlist_create(user=user_id, name=playlist_name, public=False)
+playlist_uri = create_playlist_response['uri']
+print(playlist_uri)
